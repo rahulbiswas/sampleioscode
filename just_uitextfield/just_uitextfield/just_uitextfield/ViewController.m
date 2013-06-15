4c0ef83138cf84b786ecaec8b9d2c86ee3a87fc1
Rahul Biswas <rahul@redwatermelon.com>
2013-06-15 16:10:31 -0700

    adding two buttons and a uilabel

--- just_uitextfield/just_uitextfield/just_uitextfield/ViewController.m
+++ just_uitextfield/just_uitextfield/just_uitextfield/ViewController.m
@@ -2,20 +2,38 @@
 
 @implementation ViewController
 
+- (void) button0Pressed {
+    NSLog(@"button0Pressed");
+}
+
+- (void) button1Pressed {
+    NSLog(@"button1Pressed");
+}
+
 - (void)loadView {
 	[super loadView];
     
-    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(10, 200, 300, 40)];
-    textField.borderStyle = UITextBorderStyleRoundedRect;
-    textField.font = [UIFont systemFontOfSize:15];
-    textField.placeholder = @"enter text";
-    textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    textField.keyboardType = UIKeyboardTypeDefault;
-    textField.returnKeyType = UIReturnKeyDone;
-    textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-    textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
-    textField.delegate = nil;
-    [self.view addSubview:textField];
+    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 40)];
+    label.text = @"Professor Pepper";
+    label.font = [UIFont systemFontOfSize:20];
+    label.textColor = [UIColor yellowColor];
+    label.backgroundColor = [UIColor blackColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
+    
+    UIButton* button0 = [[UIButton alloc] initWithFrame:CGRectMake(20, 70, 80, 40)];
+    [button0 setTitle:@"Off" forState:UIControlStateNormal];
+    button0.backgroundColor = [UIColor orangeColor];
+    button0.titleLabel.font = [UIFont systemFontOfSize:20];
+    [button0 addTarget:self action:@selector(button0Pressed) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:button0];
+    
+    UIButton* button1 = [[UIButton alloc] initWithFrame:CGRectMake(220, 70, 80, 40)];
+    [button1 setTitle:@"On" forState:UIControlStateNormal];
+    button1.backgroundColor = [UIColor orangeColor];
+    button1.titleLabel.font = [UIFont systemFontOfSize:20];
+    [button1 addTarget:self action:@selector(button1Pressed) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:button1];
 }
 
 - (void)didReceiveMemoryWarning {
