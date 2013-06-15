d22d8602a71fb2868d0f5146cf4420373c6a6caf
Rahul Biswas <rahul@redwatermelon.com>
2013-06-15 16:23:50 -0700

    two simple animations

--- just_uitextfield/just_uitextfield/just_uitextfield/ViewController.h
+++ just_uitextfield/just_uitextfield/just_uitextfield/ViewController.h
@@ -3,5 +3,8 @@
 @interface ViewController : UIViewController {
 }
 
+@property (nonatomic, retain) UILabel* label;
+@property (nonatomic, retain) UILabel* dancingR;
+
 @end
 
--- just_uitextfield/just_uitextfield/just_uitextfield/ViewController.m
+++ just_uitextfield/just_uitextfield/just_uitextfield/ViewController.m
@@ -4,22 +4,40 @@
 
 - (void) button0Pressed {
     NSLog(@"button0Pressed");
+    [UIView animateWithDuration:1.0 animations:^{
+        _label.alpha = 0.0;
+    }];
+    [UIView animateWithDuration:0.5 animations:^{
+        _dancingR.frame = CGRectMake(20, 120, 50, 50);
+    }];
 }
 
 - (void) button1Pressed {
     NSLog(@"button1Pressed");
+    [UIView animateWithDuration:1.0 animations:^{
+        _label.alpha = 1.0;
+    }];
+    [UIView animateWithDuration:0.5 animations:^{
+        _dancingR.frame = CGRectMake(300, 120, 50, 50);
+    }];
 }
 
 - (void)loadView {
 	[super loadView];
     
-    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 40)];
-    label.text = @"Professor Pepper";
-    label.font = [UIFont systemFontOfSize:20];
-    label.textColor = [UIColor yellowColor];
-    label.backgroundColor = [UIColor blackColor];
-    label.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:label];
+    _label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 40)];
+    _label.text = @"Professor Pepper";
+    _label.font = [UIFont systemFontOfSize:20];
+    _label.textColor = [UIColor yellowColor];
+    _label.backgroundColor = [UIColor blackColor];
+    _label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_label];
+    
+    _dancingR = [[UILabel alloc] initWithFrame:CGRectMake(20, 120, 50, 50)];
+    _dancingR.text = @"R";
+    _dancingR.backgroundColor = [UIColor clearColor];
+    _dancingR.textColor = [UIColor redColor];
+    [self.view addSubview:_dancingR];
     
     UIButton* button0 = [[UIButton alloc] initWithFrame:CGRectMake(20, 70, 80, 40)];
     [button0 setTitle:@"Off" forState:UIControlStateNormal];
